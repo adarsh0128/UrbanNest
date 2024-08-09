962240e71845f4709ace005d56fae3e61c439bc4
Adarsh kumar <adarshkumar0128@gmail.com>
2024-08-10 01:48:49 +0530

    47.4

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 // connection with database
 const MONGO_URL = "mongodb://127.0.0.1:27017/UrbanNest";
@@ -18,20 +22,32 @@ app.get("/", function (req, res) {
   res.send("I am adarsh");
 });
 
-//initial database connection
-app.get("/testListing", async function (req, res) {
-  let sampleListing = new Listing({
-    title: "My new Villa",
-    description: "By my beach",
-    price: 1200,
-    location: "calangute , Goa",
-    country: "India",
-  });
-
-  await sampleListing.save();
-  console.log("sample saved");
-  res.send("succesful testing");
+// Index Route
+app.get("/listings", async function (req, res) {
+  const allListings = await Listing.find({});
+  res.render("./listings/index.ejs", { allListings });
 });
+
+// Index Route
+app.get("/listings", async function (req, res) {
+  const allListings = await Listing.find({});
+  res.render("./listings/index.ejs", { allListings });
+});
+
+//initial database connection
+// app.get("/testListing", async function (req, res) {
+//   let sampleListing = new Listing({
+//     title: "My new Villa",
+//     description: "By my beach",
+//     price: 1200,
+//     location: "calangute , Goa",
+//     country: "India",
+//   });
+
+//   await sampleListing.save();
+//   console.log("sample saved");
+//   res.send("succesful testing");
+// });
 
 app.listen(8080, () => {
   console.log("server is listening");
